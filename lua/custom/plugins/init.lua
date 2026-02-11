bd6daea6a5983149564cab8188cabfa59c9defc2
Philipp Thaler <philipp@thaler.fyi>
2026-02-11 23:05:20 +0100

    add Neogit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,34 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'NeogitOrg/neogit',
+    lazy = true,
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+      'ibhagwan/fzf-lua', -- optional
+      'nvim-mini/mini.pick', -- optional
+      'folke/snacks.nvim', -- optional
+    },
+    cmd = 'Neogit',
+    keys = {
+      { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Show Neogit UI' },
+    },
+  },
+  {
+    'windwp/nvim-ts-autotag',
+    ft = { 'html', 'javascriptreact', 'typescriptreact', 'xml' },
+    config = function()
+      require('nvim-ts-autotag').setup {
+        -- Optional: You can add configuration options here
+        -- For example, to disable closing for self-closing tags (e.g., <img> in HTML)
+        -- disable_autotag_for_filetypes = { "xml" }, -- Example of disabling for specific filetypes
+      }
+    end,
+  },
+}
