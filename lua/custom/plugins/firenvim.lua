71a9b9f3ddde57d9ab86efc64ac3fe40aa98ceff
Philipp Thaler <philipp@thaler.fyi>
2026-08-29 21:14:32 +0200

    Add firenvim config

--- lua/custom/plugins/firenvim.lua
+++ lua/custom/plugins/firenvim.lua
@@ -1,4 +1,12 @@
 return {
   'glacambre/firenvim',
   lazy = false,
+  config = function()
+    vim.g.firenvim_config = {
+      globalSettings = {},
+      localSettings = {
+        ['.*'] = { takeover = 'never' },
+      },
+    }
+  end,
 }
